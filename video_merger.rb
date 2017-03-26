83490a0e83f3138cd2a26c4b480dc3b9e6fb77bd
Dave Jones <david@dvj.me.uk>
2017-03-26 20:13:33 +0100

    Convert from instance to class methods

--- video_merger.rb
+++ video_merger.rb
@@ -5,7 +5,7 @@ OUTPUT_NAME = "merged.MP4"
 DRY_RUN = false
 
 class VideoMerger
-  def merge
+  def self.merge
     files = find_files_to_merge
     input_list = create_input_list(files)
     do_merge(input_list) unless DRY_RUN
@@ -13,7 +13,7 @@ class VideoMerger
   end
 
   private
-  def create_input_list(files)
+  def self.create_input_list(files)
     list_path = File.join(Dir.pwd, "input.list")
     File.open(list_path, "w") do |list|
       files.each do |file|
@@ -23,19 +23,19 @@ class VideoMerger
     list_path
   end
 
-  def find_files_to_merge
+  def self.find_files_to_merge
     glob_pattern = File.join(Dir.pwd, "*#{VIDEO_EXTENSION}")
     files = Dir.glob(glob_pattern)
     files.sort_by{|file| File.mtime(file)}
   end
 
-  def do_merge(input_list)
+  def self.do_merge(input_list)
     output_path = File.join(Dir.pwd, OUTPUT_NAME)
     command = "ffmpeg -safe 0 -f concat -i '#{input_list}' -c copy '#{output_path}'"
     Kernel.system(command)
   end
 
-  def do_cleanup(input_list)
+  def self.do_cleanup(input_list)
     File.delete(input_list)
   end
 end
@@ -43,7 +43,7 @@ end
 # Entrypoint
 if __FILE__ == $0
   if ENV['STY'] # if running in a screen session
-    VideoMerger.new.merge
+    VideoMerger.merge
   else
     puts "It is recommended that you run this script within a screen session."
   end
