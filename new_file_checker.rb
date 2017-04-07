61069e72880900891a5a34824cd4d7cf08110451
Dave Jones <david@dvj.me.uk>
2017-04-07 20:39:37 +0100

    Adding tool for verifying the presence of a new file in a target directory

---
+++ new_file_checker.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+class NewFileChecker
+  # The directory to be checked for the presence of new file(s)
+  CHECK_DIRECTORY="/home/pi/backup"
+  # The maximum accepted time since last file creation. Exceeding this limit results in a check failure.
+  CHECK_LIMIT=86400 #seconds (24 hours)
+
+  def self.check
+    creation_dates = Dir.entries(CHECK_DIRECTORY).map do |entry|
+      next if [".",".."].include?(entry) # we're not interested in the navigation symbolic links
+      absolute_path = File.join(CHECK_DIRECTORY, entry)
+      File.ctime(absolute_path)
+    end.compact
+
+    max_creation_date = creation_dates.max
+    seconds_since_creation = (Time.now - max_creation_date).to_i
+
+    if seconds_since_creation < CHECK_LIMIT
+      puts "PASS: Latest file created #{max_creation_date}"
+      exit(0)
+    else
+      puts "FAIL: Latest file created #{max_creation_date}"
+      exit(1)
+    end
+  end
+end
+
+if __FILE__ == $0
+  NewFileChecker.check
+end
