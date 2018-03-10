ddb7960d1ac5d3170a79cd2d8d43fd3c91ab43eb
Dave J <david@dvj.me.uk>
2018-03-10 13:05:38 +0000

    Make PasswordTokenizer executable from command-line

--- password_tokenizer.rb
+++ password_tokenizer.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
 
 # Simple helper class to select specific tokens from a longer, pseudorandom password.
+require "pry"
 class PasswordTokenizer
 
   # PUBLIC
-  # Takes a password and unlimited number of indexes, returning the tokens from each index
-  # tokenize("mypassword", 1, 2, 3) => m y p
-  def self.tokenize(password, *indexes)
+  # Takes a password and array of indexes, returning the tokens from each index
+  # tokenize("mypassword", [1, 2, 3]) => m y p
+  def self.tokenize(password, indexes)
     indexes.each do |index|
       puts "#{index}: #{password[index - 1]}"
     end;nil
   end
+end
+
+# Entrypoint
+if __FILE__ == $0
+  password, *indexes = ARGV
+  PasswordTokenizer.tokenize(password, indexes.map(&:to_i))
 end
