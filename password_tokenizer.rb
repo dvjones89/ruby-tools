afb18e8c17589eb182b09102f2a1423d1f8b294a
Dave J <david@dvj.me.uk>
2018-03-13 22:53:33 +0000

    Correct code comments to use single quotes, rather than double quotes. All strings passed from the shell should be enclosed within single quotes, else you risk the shell substituting (expanding) the characters before the string is passed to Ruby. https://stackoverflow.com/questions/48687023/strange-behavior-of-argv-when-passing-string-containing

--- password_tokenizer.rb
+++ password_tokenizer.rb
@@ -5,7 +5,7 @@ class PasswordTokenizer
 
   # PUBLIC
   # Takes a password and array of indexes, returning the tokens from each index
-  # tokenize("mypassword", [1, 2, 3]) => m y p
+  # tokenize('mypassword', [1, 2, 3]) => m y p
   def self.tokenize(password, indexes)
     indexes.each do |index|
       puts "#{index}: #{password[index - 1]}"
@@ -14,7 +14,7 @@ class PasswordTokenizer
 end
 
 # Entrypoint
-# password_tokenizer.rb "mypassword", 1,2,3 => m y p
+# password_tokenizer.rb 'mypassword', 1,2,3 => m y p
 if __FILE__ == $0
   password, indexes = ARGV
   PasswordTokenizer.tokenize(password, indexes.split(",").map(&:to_i))
