2d3e016960f6605f21c3c82ea1888b3ca506491a
Dave J <david@dvj.me.uk>
2018-05-26 16:13:00 +0100

    Adding subject for passing "l" index for "last" character and "p" index for "penultimate" character.

--- password_tokenizer.rb
+++ password_tokenizer.rb
@@ -6,16 +6,26 @@ class PasswordTokenizer
   # PUBLIC
   # Takes a password and array of indexes, returning the tokens from each index
   # tokenize('mypassword', [1, 2, 3]) => m y p
+  # You can also pass index "l" for "last" and"p" for "penultimate"
   def self.tokenize(password, indexes)
+
     indexes.each do |index|
-      puts "#{index}: #{password[index - 1]}"
-    end;nil
-  end
-end
+      if index.downcase == "l"
+        index = password.length
+      elsif index.downcase == "p"
+        index = password.length - 1
+      end
+
+       # Remember that string characters are indexed from zero, therefore we subject one from index
+       index = index.to_i - 1
+       puts "#{index}: #{password[index]}"
+     end;nil
+   end
+ end
 
 # Entrypoint
 # password_tokenizer.rb 'mypassword', 1,2,3 => m y p
 if __FILE__ == $0
   password, indexes = ARGV
-  PasswordTokenizer.tokenize(password, indexes.split(",").map(&:to_i))
+  PasswordTokenizer.tokenize(password, indexes.split(","))
 end
