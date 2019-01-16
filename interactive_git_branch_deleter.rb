f5396a83b140eb266e6de202e36aff85c5360e3e
Dave J <david@dvj.me.uk>
2019-01-16 12:23:22 +0000

    Add InteractiveGitBranchDeleter tool
    Over time I find that my local git branches pile up, making it harder to
    move between the branches I'm interested in. A quick Google search
    revealed several scripts to delete branches that have been merged into
    master, which helped to a degree, but doesn't help with those branches
    that never made it to production.
    
    This simple ruby script iterates over the git branches in your current
    working directly and gives the option to delete each branch in turn,
    making it easier to quickly deleted unwanted branches from git with
    minimal keystrokes and effort.

---
+++ interactive_git_branch_deleter.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+# Simple helper class to iterate over the git branches in your working directory and offer to delete them.
+class InteractiveGitBranchDeleter
+  require 'open3'
+
+  def self.perform
+    stdout, stderr, status = Open3.capture3("git branch")
+    branches = stdout.split
+
+    branches.each do |branch|
+      puts "\n Delete \e[31m#{branch}?\e[0m (y/n)"
+      response = gets.chomp.strip.downcase
+
+      if response == 'y'
+        command = "git branch -d #{branch}"
+        stdout, stderr, status = Open3.capture3(command)
+      end
+    end
+  end
+end
+
+# Entrypoint
+if __FILE__ == $0
+  InteractiveGitBranchDeleter.perform
+end
